5b94dfa3e108d6bb708c353a2f6c621acc96d1e9
mahakamal-e <mahakamal09@gmail.com>
2025-09-19 03:54:19 +0300

    feat: add viewsets for Movie, Genre, and Favorite

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Movie, Genre, Favorite
+from .serializers import MovieSerializer, GenreSerializer, FavoriteSerializer
 
-# Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class FavoriteViewSet(viewsets.ModelViewSet):
+    queryset = Favorite.objects.all()
+    serializer_class = FavoriteSerializer

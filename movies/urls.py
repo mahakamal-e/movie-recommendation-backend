8124592671f5459737269c70ff85933c0ee7439b
mahakamal-e <mahakamal09@gmail.com>
2025-09-22 00:37:34 +0300

    feat: add URL routes for movies and user favorites

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import TrendingMoviesView, UserFavoritesView
+from .views import TrendingMoviesView, UserFavoritesView, RecommendedMoviesView, SearchMoviesView
 
 
 urlpatterns = [
     path("movies/trending/", TrendingMoviesView.as_view(), name="movies_trending"),
+    path("movies/recommended/", RecommendedMoviesView.as_view(), name="movies_recommended"),
+    path("movies/search/", SearchMoviesView.as_view(), name="movies_search"),
+
     path("users/favorites/", UserFavoritesView.as_view(), name="user_favorites"),
+    
 ]
